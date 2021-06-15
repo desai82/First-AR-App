29bb06a61448b07b414fa511f7b7d05f07e79cd0
Nisarg <desai82@uwindor.ca>
2021-06-15 15:27:42 +0530

    First AR App- new change

--- app/src/main/java/com/example/arapp/MainActivity.java
+++ app/src/main/java/com/example/arapp/MainActivity.java
@@ -8,13 +8,20 @@ import android.os.Bundle;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Camera;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ArFragment arFragment;
+    private Scene scene;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +29,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        CustomArFragment arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
-        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent)->{
-        Anchor anchor = hitResult.createAnchor();
+        scene = arFragment.getArSceneView().getScene();
+        camera = scene.getCamera();
 
+        addDummytoScene();
+//
+//        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent)->{
+//        Anchor anchor = hitResult.createAnchor();
+//
+//        ModelRenderable.builder()
+//                .setSource(this, Uri.parse("Geishamasks.sfb"))
+//                .build()
+//                .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+//                .exceptionally(throwable -> {
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                    builder.setMessage(throwable.getMessage())
+//                            .show();
+//                    return null;
+//                });
+//        });
+    }
+
+    private void addDummytoScene() {
         ModelRenderable.builder()
-                .setSource(this, Uri.parse("Geishamasks.sfb"))
+                .setSource(this, Uri.parse("human_male.sfb"))
                 .build()
-                .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
-                .exceptionally(throwable -> {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setMessage(throwable.getMessage())
-                            .show();
-                    return null;
-                });
-        });
-    }
+                .thenAccept( renderable -> {
+                    Node node =new Node();
+                    node.setRenderable(renderable);
+                    scene.addChild(node);
 
-    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
-        AnchorNode anchorNode = new AnchorNode(anchor);
-        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
-        transformableNode.setParent(anchorNode);
-        transformableNode.setRenderable(modelRenderable);
-        arFragment.getArSceneView().getScene().addChild(anchorNode);
-        transformableNode.select();
+                    Random random = new Random();
+                    int x = random.nextInt(10);
+                    int z = random.nextInt(10);
+                    int y = random.nextInt(20);
+                    node.setWorldPosition(new Vector3((float) x,(float)y ,(float) z));
+                });
+
     }
+//
+//    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+//        AnchorNode anchorNode = new AnchorNode(anchor);
+//        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+//        transformableNode.setParent(anchorNode);
+//        transformableNode.setRenderable(modelRenderable);
+//        arFragment.getArSceneView().getScene().addChild(anchorNode);
+//        transformableNode.select();
+//    }
 }
